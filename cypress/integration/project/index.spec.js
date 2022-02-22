4f933ff218dd34f9c4990b3c056f3f93def475a7
Mael Jamin <mael.jamin@gmail.com>
2022-02-22 20:07:33 +0100

    add table test index.blade.php

--- cypress/integration/project/index.spec.js
+++ cypress/integration/project/index.spec.js
@@ -7,10 +7,26 @@ describe('indexProjects', () => {
     })
 
     it('should display_table', function () {
-        cy.get('table').should('have.class','table-striped table-bordered table-responsive-lg');
+        const table = cy.get('table');
+        table.should('have.class','table-striped table-bordered table-responsive-lg');
+
+        const thead = table.children('thead');
+        thead.should('be.visible');
+        thead.get('tr[id="tableHeader"] > th').should('have.length',7);
+
+        const tbody = table.children('tbody');
+        tbody.should('be.visible');
+        tbody.get('tr').should('have.length.above',0);
+        tbody.get('tr').should('have.length.below',6);
+        /*
+        tbody.contains('tr').each(element => {
+           element
+        });*/
+        //tbody.contains('tr').should('have.length.above',0);
+        //tbody.contains('tr').should('have.length.below',3);
     });
 
     it('should have_pagination', function () {
-        cy.get('nav').should('have.','role','navigation')
+        cy.get('nav').should('have.attr','role','navigation')
     });
 });
--- resources/views/projects/index.blade.php
+++ resources/views/projects/index.blade.php
@@ -21,45 +21,49 @@
     @endif
 
     <table class="table table-striped table-bordered table-responsive-lg">
-        <tr>
-            <th style="width: 5%;">N°</th>
-            <th style="width: 20%;">Nom</th>
-            <th style="width: 20%;">Résumé</th>
-            <th style="width: 20%">Lieux</th>
-            <th style="width: 5%">Coût (€)</th>
-            <th style="width: 15%">Date de création</th>
-            <th style="width: 15%">Action</th>
-        </tr>
-        @foreach ($projects as $project)
-            <tr>
-                <td>{{ ++$i }}</td>
-                <td>{{ $project->name }}</td>
-                <td>{{ $project->introduction }}</td>
-                <td>{{ $project->location }}</td>
-                <td>{{ $project->cost }}</td>
-                <td>{{ date_format($project->created_at, 'jS M Y') }}</td>
-                <td>
-                    <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
-
-                        <a href="{{ route('projects.show', $project->id) }}" title="show">
-                            <i class="fas fa-eye text-success  fa-lg"></i>
-                        </a>
-
-                        <a href="{{ route('projects.edit', $project->id) }}">
-                            <i class="fas fa-edit  fa-lg"></i>
-
-                        </a>
-
-                        @csrf
-                        @method('DELETE')
-
-                        <button type="submit" title="delete" style="border: none; background-color:transparent;">
-                            <i class="fas fa-trash fa-lg text-danger"></i>
-                        </button>
-                    </form>
-                </td>
+        <thead>
+            <tr id="tableHeader">
+                <th style="width: 5%;">N°</th>
+                <th style="width: 20%;">Nom</th>
+                <th style="width: 20%;">Résumé</th>
+                <th style="width: 20%">Lieux</th>
+                <th style="width: 5%">Coût (€)</th>
+                <th style="width: 15%">Date de création</th>
+                <th style="width: 15%">Action</th>
             </tr>
-        @endforeach
+        </thead>
+        <tbody>
+            @foreach ($projects as $project)
+                <tr id="project_{{$project['id']}}">
+                    <td>{{ ++$i }}</td>
+                    <td>{{ $project->name }}</td>
+                    <td>{{ $project->introduction }}</td>
+                    <td>{{ $project->location }}</td>
+                    <td>{{ $project->cost }}</td>
+                    <td>{{ date_format($project->created_at, 'jS M Y') }}</td>
+                    <td>
+                        <form action="{{ route('projects.destroy', $project->id) }}" method="POST">
+
+                            <a href="{{ route('projects.show', $project->id) }}" title="show">
+                                <i class="fas fa-eye text-success  fa-lg"></i>
+                            </a>
+
+                            <a href="{{ route('projects.edit', $project->id) }}">
+                                <i class="fas fa-edit  fa-lg"></i>
+
+                            </a>
+
+                            @csrf
+                            @method('DELETE')
+
+                            <button type="submit" title="delete" style="border: none; background-color:transparent;">
+                                <i class="fas fa-trash fa-lg text-danger"></i>
+                            </button>
+                        </form>
+                    </td>
+                </tr>
+            @endforeach
+        </tbody>
     </table>
 
     {!! $projects->links() !!}
